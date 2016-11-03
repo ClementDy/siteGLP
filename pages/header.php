28a50865362be8d8f8c133a5d79e764efee217d6
ClementDy <clement.dilly@etudiant.univ-lille1.fr>
2016-11-03 17:14:34 +0100

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -18,7 +18,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title>GLP - DigiTeam</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
