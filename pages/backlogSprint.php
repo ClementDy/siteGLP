542ff275d6c7995e3bec218826c6d38083e76761
ClementDy <dilly.clement@gmail.com>
2016-11-03 12:55:53 +0100

    Modification sprint + backLog Sprint

--- pages/backlogSprint.php
+++ pages/backlogSprint.php
@@ -38,16 +38,24 @@
                                                     <table class="table table-striped table-bordered table-hover">
                                                         <thead>
                                                             <tr>
-                                                                <th>Date</th>
-                                                                <th>Temps passé</th>
                                                                 <th>T&acirc;che</th>
+                                                                <th>Temps estim&eacute;</th>
+                                                                <th>Temps mis</th>
+                                                                <th>Avancement</th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
                                                             <tr>
+                                                                <td>Réaliser les cas utilisations</td>
+                                                                <td>2 heures</td>
                                                                 <td></td>
+                                                                <td>Termin&eacute;</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Rédiger le dossier (Description des besoins techniques/ergonomiques..)</td>
+                                                                <td>4 heures</td>
                                                                 <td></td>
-                                                                <td></td>
+                                                                <td>Termin&eacute;</td>
                                                             </tr>
                                                         </tbody>
                                                     </table>
--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
                
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 15 heures</p>
+                    <p style="text-align:right"> Temps Total : 21 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Cl&eacute;ment
@@ -30,7 +30,7 @@
                                     </div>
                                     <div id="sprint1" class="panel-collapse collapse">
 
-                                        <p style="text-align:right"> Temps Sprint : 1 heure</p>
+                                        <p style="text-align:right"> Temps Sprint : 7 heure</p>
                                         <div class="panel panel-default">
                                             <div class="panel-heading">
                                                 Relev&eacute; d'activit&eacute;s
@@ -47,6 +47,16 @@
                                                             </tr>
                                                         </thead>
                                                         <tbody>
+                                                            <tr>
+                                                                <td>31/10/2016 - 02/11/2016</td>
+                                                                <td>5 heure</td>
+                                                                <td>Réalisation du dossier d'expression des besoins.</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>31/10/2016 - 02/11/2016</td>
+                                                                <td>1 heure</td>
+                                                                <td>Brain storming pour dossier d'expression des besoins.</td>
+                                                            </tr>
                                                             <tr>
                                                                 <td>21/10/2016</td>
                                                                 <td>1 heure</td>
