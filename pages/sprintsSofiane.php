b0b04f371d8db8d90e5ee5dda0c123e711bc8e79
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2016-11-07 19:24:21 +0100

    ajout des sprints dans le tableau

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
     include("header.php");
 
@@ -8,10 +8,10 @@
             <div class="row">
                 <div class="col-lg-12" style="text-align:center">
                     <h1 class="page-header">Sprints - Sofiane</h1>
-               
+
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 0</p>
+                    <p style="text-align:right"> Temps Total : 8 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -19,13 +19,13 @@
                         <!-- .panel-heading -->
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
-                                
+
 
                             	<!-- Sprint 1 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint1">X - A venir</a>
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint1">1 - Réalisation du dossier d'expression des besoins </a>
                                         </h4>
                                     </div>
                                     <div id="sprint1" class="panel-collapse collapse">
@@ -47,11 +47,63 @@
                                                         </thead>
                                                         <tbody>
                                                             <tr>
-                                                                <td></td>
-                                                                <td></td>
-                                                                <td></td>
+                                                                <td>21/10/2016</td>
+                                                                <td>3 heures</td>
+                                                                <td>Installation de l'infrastructure du projet et brainstorming en équipe autour de la compréhension du sujet et des premières idées de fonctionnalités</td>
                                                             </tr>
                                                         </tbody>
+                                                        <tr>
+                                                            <th>Date</th>
+                                                            <th>Temps passé</th>
+                                                            <th>T&acirc;che</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>28/10/2016</td>
+                                                            <td>2 heures</td>
+                                                            <td>Réalisation des cas d'utilisation concernant le dossier d'expression des besoins</td>
+                                                        </tr>
+                                                    </tbody>
+                                                    <tr>
+                                                        <th>Date</th>
+                                                        <th>Temps passé</th>
+                                                        <th>T&acirc;che</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>02/11/2016</td>
+                                                        <td>1 heures 30 </td>
+                                                        <td>Relecture et correction concernant le dossier d'expression des besoins</td>
+                                                    </tr>
+                                                </tbody>
+                                                <tr>
+                                                    <th>Date</th>
+                                                    <th>Temps passé</th>
+                                                    <th>T&acirc;che</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>04/11/2016</td>
+                                                    <td>1 heures 30</td>
+                                                    <td>Review en équipe et avec le client à propos du dossier d'expression des besoins</td>
+                                                </tr>
+                                            </tbody>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Temps passé</th>
+                                                <th>T&acirc;che</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>07/11/2016</td>
+                                                <td>3 heures</td>
+                                                <td>Installation de l'infrastructure du projet et brainstorming en équipe autour de la compréhension du sujet et des premières idées de fonctionnalités</td>
+                                            </tr>
+                                        </tbody>
                                                     </table>
                                                 </div>
                                                 <!-- /.table-responsive -->
