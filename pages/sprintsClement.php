76598ade1992c6dba87346e8d92a013e9d858f76
ClementDy <dilly.clement@gmail.com>
2016-11-15 13:00:47 +0100

    Modification SprintClément pour le sprint 2

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
                
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 21 heures</p>
+                    <p style="text-align:right"> Temps Total : 25 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Cl&eacute;ment
@@ -20,6 +20,53 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+
+                                <!-- Sprint 2 -->
+                              <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">2 - Modification du dossier d'expression des besoins (V1.2)</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint2" class="panel-collapse collapse">
+
+                                        <p style="text-align:right"> Temps Sprint : 4 heure</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps passé</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>15/11/2016</td>
+                                                                <td>2 heure</td>
+                                                                <td>Réalisation du schéma hiérarchique</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>14/11/2016</td>
+                                                                <td>2 heure</td>
+                                                                <td>Modification des CU et réalisation du diagramme d'activité "Deposer une offre"</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>    
+
 
                             	<!-- Sprint 1 -->
                               <div class="panel panel-default">
