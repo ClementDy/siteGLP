9ee65a6ca3ec7f743f7f4835b050fca747501420
ClementDy <dilly.clement@gmail.com>
2016-11-24 19:23:04 +0100

    Modification du sprintClement (3)

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 	               
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 31 heures</p>
+	                    <p style="text-align:right"> Temps Total : 34 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -19,18 +19,16 @@
 	                        <!-- .panel-heading -->
 	                        <div class="panel-body">
 	                            <div class="panel-group" id="accordion">
-	                                
-	
-	                                <!-- Sprint 2 -->
+	    <!-- Sprint 3 -->
 	                              <div class="panel panel-default">
 	                                    <div class="panel-heading">
 	                                        <h4 class="panel-title">
-	                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">2 - Modification du dossier d'expression des besoins (V1.2)</a>
+	                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">3 - </a>
 	                                        </h4>
 	                                    </div>
 	                                    <div id="sprint2" class="panel-collapse collapse">
 	
-	                                        <p style="text-align:right"> Temps Sprint : 10 heure</p>
+	                                        <p style="text-align:right"> Temps Sprint : 1 heure</p>
 	                                        <div class="panel panel-default">
 	                                            <div class="panel-heading">
 	                                                Relev&eacute; d'activit&eacute;s
@@ -47,6 +45,56 @@
 	                                                            </tr>
 	                                                        </thead>
 	                                                        <tbody>
+																<tr>
+	                                                                <td>24/11/2016</td>
+	                                                                <td> 1 heures</td>
+	                                                                <td>Réalisation de maquettes sur balsamiq (Créer une candidature)</td>
+	                                                            </tr>
+
+	
+	                                                        </tbody>
+	                                                    </table>
+	                                                </div>
+	                                                <!-- /.table-responsive -->
+	                                            </div>
+	                                            <!-- /.panel-body -->
+	                                        </div>
+	                                    </div>
+	                                </div>    
+	
+
+	
+	                                <!-- Sprint 2 -->
+	                              <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        <h4 class="panel-title">
+	                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">2 - Modification du dossier d'expression des besoins (V1.2)</a>
+	                                        </h4>
+	                                    </div>
+	                                    <div id="sprint2" class="panel-collapse collapse">
+	
+	                                        <p style="text-align:right"> Temps Sprint : 12 heure</p>
+	                                        <div class="panel panel-default">
+	                                            <div class="panel-heading">
+	                                                Relev&eacute; d'activit&eacute;s
+	                                            </div>
+	                                            <!-- /.panel-heading -->
+	                                            <div class="panel-body">
+	                                                <div class="table-responsive">
+	                                                    <table class="table table-striped table-bordered table-hover">
+	                                                        <thead>
+	                                                            <tr>
+	                                                                <th>Date</th>
+	                                                                <th>Temps passé</th>
+	                                                                <th>T&acirc;che</th>
+	                                                            </tr>
+	                                                        </thead>
+	                                                        <tbody>
+	                                                        	<tr>
+	                                                                <td>16/11/2016</td>
+	                                                                <td> 2 heures</td>
+	                                                                <td>Finalisation du socle technique. </td>
+	                                                            </tr>
 																<tr>
 	                                                                <td>16/11/2016</td>
 	                                                                <td> 2 heures</td>
