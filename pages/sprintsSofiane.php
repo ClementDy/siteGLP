58beb63851f7dfb377de2f499940e77f7472f013
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2016-11-24 18:58:36 +0100

    Ajout sprint 0 et sprint 2

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 8 heures</p>
+                    <p style="text-align:right"> Temps Total : 14 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -20,7 +20,36 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
 
-
+                              <!-- Sprint 0 -->
+                              <div class="panel panel-default">
+                                  <div class="panel-heading">
+                                      <h4 class="panel-title">
+                                          <a data-toggle="collapse" data-parent="#accordion" href="#sprint0">0 - Réalisation du site WEB </a>
+                                      </h4>
+                                  </div>
+                                  <div id="sprint0" class="panel-collapse collapse">
+                                      <p style="text-align:right"> Temps Sprint : 0</p>
+                                      <div class="panel panel-default">
+                                          <div class="panel-heading">
+                                              Relev&eacute; d'activit&eacute;s
+                                          </div>
+                                          <!-- /.panel-heading -->
+                                          <div class="panel-body">
+                                              <div class="table-responsive">
+                                                  <table class="table table-striped table-bordered table-hover">
+                                                      <thead>
+                                                          <tr>
+                                                              <th>Date</th>
+                                                              <th>Temps passé</th>
+                                                              <th>T&acirc;che</th>
+                                                          </tr>
+                                                      </thead>
+                                                    </table>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
                             	<!-- Sprint 1 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
@@ -76,6 +105,54 @@
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Sprint 2 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">2 - Modification du dossier d'expression des besoins (V1.2) </a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint2" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 6</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps passé</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+
+                                                        <tbody>
+                                                          <tr>
+                                                              <td>18/11/2016</td>
+                                                              <td>2 heures</td>
+                                                              <td>Différents travaux sur le socle technique (Récuperation des données en base via springboot)</td>
+                                                          </tr>
+                                                          <tr>
+                                                              <td>16/11/2016</td>
+                                                              <td>2 heures</td>
+                                                              <td>Réalisation des schémas CU</td>
+                                                          </tr>
+                                                          <tr>
+                                                              <td>15/11/2016</td>
+                                                              <td>2 heures</td>
+                                                              <td>Modifications du dossier d'expression des besoins, corrections</td>
+                                                          </tr>
+                                                        </tbody>
+                                                      </table>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
                             </div>
                         </div>
                         <!-- .panel-body -->
