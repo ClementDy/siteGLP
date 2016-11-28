2f020f4d7082cc852f8358ef023248490805c9bd
CedricDuhautois <c.duhautois@gmail.com>
2016-11-28 21:30:44 +0100

    Temps passé sprints 0 et 2

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 9 heures</p>
+                    <p style="text-align:right"> Temps Total : 14 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,16 +20,59 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 2 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint2">2 - Modification du dossier d'expression des besoins (V1.2)</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint2" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 5 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>16/11/2016</td>
+                                                                <td>1 heure</td>
+                                                                <td>Travaux sur le socle technique</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>14/11/2016 - 16/11/2016</td>
+                                                                <td>4 heures</td>
+                                                                <td>Correction et finalisation du dossier d'expression des besoins</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
 
                             	<!-- Sprint 1 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint1">1 - Réalisation du dossier d'expression des besoins</a>
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint1">1 - R&eacute;alisation du dossier d'expression des besoins</a>
                                         </h4>
                                     </div>
                                     <div id="sprint1" class="panel-collapse collapse">
-
                                         <p style="text-align:right"> Temps Sprint : 9 heures</p>
                                         <div class="panel panel-default">
                                             <div class="panel-heading">
@@ -42,7 +85,7 @@
                                                         <thead>
                                                             <tr>
                                                                 <th>Date</th>
-                                                                <th>Temps passé</th>
+                                                                <th>Temps pass&eacute;</th>
                                                                 <th>T&acirc;che</th>
                                                             </tr>
                                                         </thead>
@@ -50,17 +93,17 @@
                                                             <tr>
                                                                 <td>04/11/2016</td>
                                                                 <td>1 heure 30</td>
-                                                                <td>Review, en équipe et avec le client, du dossier d'expression des besoins</td>
+                                                                <td>Review, en &eacute;quipe et avec le client, du dossier d'expression des besoins</td>
                                                             </tr>
                                                             <tr>
                                                                 <td>29/10/2016 - 02/11/2016</td>
                                                                 <td>4 heures</td>
-                                                                <td>Réalisation du dossier d'expression des besoins.</td>
+                                                                <td>R&eacute;alisation du dossier d'expression des besoins</td>
                                                             </tr>
                                                             <tr>
                                                                 <td>24/10/2016 - 25/10/2016</td>
                                                                 <td>2 heures</td>
-                                                                <td>Brainstorming pour dossier d'expression des besoins.</td>
+                                                                <td>Brainstorming pour dossier d'expression des besoins</td>
                                                             </tr>
                                                             <tr>
                                                                 <td>21/10/2016</td>
@@ -76,6 +119,40 @@
                                         </div>
                                     </div>
                                 </div>
+
+                                <!-- Sprint 0 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint0">0 - R&eacute;alisation du site WEB</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint0" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 0 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         <!-- .panel-body -->
