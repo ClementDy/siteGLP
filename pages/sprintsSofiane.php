d7352f32e3b58fe1376f7e3765b62db059d01997
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2016-11-29 12:13:03 +0100

    ajout sprint 3 sofiane

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 14 heures</p>
+                    <p style="text-align:right"> Temps Total : 19 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -19,6 +19,49 @@
                         <!-- .panel-heading -->
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
+                              <!--Sprint 3 -->
+                              <div class="panel panel-default">
+                                  <div class="panel-heading">
+                                      <h4 class="panel-title">
+                                          <a data-toggle="collapse" data-parent="#accordion" href="#sprint3">3 - Developpement des premières fonctionnalités</a>
+                                      </h4>
+                                  </div>
+                                  <div id="sprint3" class="panel-collapse collapse">
+                                      <p style="text-align:right"> Temps Sprint : 5</p>
+                                      <div class="panel panel-default">
+                                          <div class="panel-heading">
+                                              Relev&eacute; d'activit&eacute;s
+                                          </div>
+                                          <!-- /.panel-heading -->
+                                          <div class="panel-body">
+                                              <div class="table-responsive">
+                                                  <table class="table table-striped table-bordered table-hover">
+                                                      <thead>
+                                                          <tr>
+                                                              <th>Date</th>
+                                                              <th>Temps passé</th>
+                                                              <th>T&acirc;che</th>
+                                                          </tr>
+                                                      </thead>
+
+                                                      <tbody>
+                                                        <tr>
+                                                            <td>29/11/2016</td>
+                                                            <td>2 heures</td>
+                                                            <td>Modification du diagramme UML, modification du diagramme de classes</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>25/11/2016</td>
+                                                            <td>3 heures</td>
+                                                            <td>Redaction de la backlog, diagramme UML, modification du dossier d'expression des besoins</td>
+                                                        </tr>
+                                                      </tbody>
+                                                    </table>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
                               <!-- Sprint 2 -->
                               <div class="panel panel-default">
                                   <div class="panel-heading">
