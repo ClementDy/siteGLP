b29fc350aaf13be900d4e912dda030234bea7e3c
CedricDuhautois <c.duhautois@gmail.com>
2016-12-01 21:18:51 +0100

    Temps passé sprint 3

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 14 heures</p>
+                    <p style="text-align:right"> Temps Total : 19 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,6 +20,51 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 3 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint3">Sprint - 3</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint3" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 5 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>30/11/2016</td>
+                                                                <td>1 heure 30</td>
+                                                                <td>Modification du dossier d'expression des besoins</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>29/11/2016 - 30/11/2016</td>
+                                                                <td>3 heures 30</td>
+                                                                <td>Etude des technologies de templates utilisables dans un projet Spring Boot</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <!-- Sprint 2 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
