cf4d8434ef9e9bec2c6e8aefaef73e01912be0de
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2016-12-02 12:08:05 +0100

    Ajout sprint sofiane

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -68,7 +68,7 @@
                                       </h4>
                                   </div>
                                   <div id="sprint3" class="panel-collapse collapse">
-                                      <p style="text-align:right"> Temps Sprint : 5</p>
+                                      <p style="text-align:right"> Temps Sprint : 5 heures</p>
                                       <div class="panel panel-default">
                                           <div class="panel-heading">
                                               Relev&eacute; d'activit&eacute;s
