c351a8b3e1f1fc97e6aecd970a241a168ad9b09d
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2016-12-09 14:03:23 +0100

    Sprint Clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 40 heures</p>
+	                    <p style="text-align:right"> Temps Total : 42 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -29,7 +29,7 @@
 																			</div>
 																			<div id="sprint4" class="panel-collapse collapse">
 
-																					<p style="text-align:right"> Temps Sprint : 1 heure</p>
+																					<p style="text-align:right"> Temps Sprint : 3 heure</p>
 																					<div class="panel panel-default">
 																							<div class="panel-heading">
 																									Relev&eacute; d'activit&eacute;s
@@ -46,6 +46,11 @@
 																															</tr>
 																													</thead>
 																													<tbody>
+																														<tr>
+																															<td>09/12/2016</td>
+																															<td>2 heures</td>
+																															<td>Uniformisation des différentes parties des formulaires créés (infos perso,divers,souhait..) et réalisation du header</td>
+																														</tr>
 																														<tr>
 																															<td>06/12/2016</td>
 																															<td>1 heures</td>
