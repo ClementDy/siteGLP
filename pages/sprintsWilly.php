e702ea7e16d6c63a77f195d6e552c3e932147e49
WillyPezant <pezant.willy@gmail.com>
2016-12-18 12:14:54 +0100

    Sprint Willy - modif dossier archi

--- pages/sprintsWilly.php
+++ pages/sprintsWilly.php
@@ -11,7 +11,7 @@
                
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 23,5 heures</p>
+                    <p style="text-align:right"> Temps Total : 24 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Willy
@@ -19,6 +19,41 @@
                         <!-- .panel-heading -->
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
+							
+                                <!-- Sprint 5 -->
+                              <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint0">Sprint - 5</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint0" class="panel-collapse collapse">
+
+                                        <p style="text-align:right"> Temps Sprint : 30 min</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>18/12/2016</th>
+                                                                <th>30 min</th>
+                                                                <th>Modification schéma architecture - dossier</th>
+                                                            </tr>
+                                                        </thead>
+                                                        
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>    
                                 <!-- Sprint 4 -->
 																<div class="panel panel-default">
 																			<div class="panel-heading">
@@ -239,7 +274,7 @@
                                 </div>
 
 
-                                <!-- Sprint 2 -->
+                                <!-- Sprint 0 -->
                               <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
