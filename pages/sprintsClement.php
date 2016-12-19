0d3b03979db94ff22e8164c5dd776375f9d7cd38
ClementDy <dilly.clement@gmail.com>
2016-12-19 18:05:42 +0100

    Clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 52 heures</p>
+	                    <p style="text-align:right"> Temps Total : 54 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -19,6 +19,56 @@
 	                        <!-- .panel-heading -->
 	                        <div class="panel-body">
 	                            <div class="panel-group" id="accordion">
+
+
+
+	<!-- Sprint 4 -->
+																<div class="panel panel-default">
+																			<div class="panel-heading">
+																					<h4 class="panel-title">
+																							<a data-toggle="collapse" data-parent="#accordion" href="#sprint5">Sprint - 5</a>
+																					</h4>
+																			</div>
+																			<div id="sprint5" class="panel-collapse collapse">
+
+																					<p style="text-align:right"> Temps Sprint : 2 heure</p>
+																					<div class="panel panel-default">
+																							<div class="panel-heading">
+																									Relev&eacute; d'activit&eacute;s
+																							</div>
+																							<!-- /.panel-heading -->
+																							<div class="panel-body">
+																									<div class="table-responsive">
+																											<table class="table table-striped table-bordered table-hover">
+																													<thead>
+																															<tr>
+																																	<th>Date</th>
+																																	<th>Temps passé</th>
+																																	<th>T&acirc;che</th>
+																															</tr>
+																													</thead>
+																													<tbody>
+																														<tr>
+																															<td>19/12/2016</td>
+																															<td>2 heures</td>
+																															<td>Modification des différents formulaires pour uniformiser ces derniers. Résolution d'un problème de date avec thymeleaf. (Infos_Perso)</td>
+																														</tr>
+																														
+
+																													</tbody>
+																											</table>
+																									</div>
+																									<!-- /.table-responsive -->
+																							</div>
+																							<!-- /.panel-body -->
+																					</div>
+																			</div>
+																	</div>
+
+
+
+
+
 
   														<!-- Sprint 4 -->
 																<div class="panel panel-default">
