ea1c07fe86845b9067467a43b81ab8a893e22229
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2017-01-06 10:04:56 +0100

    modif de la page de sprint sofiane

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 26 heures</p>
+                    <p style="text-align:right"> Temps Total : 28 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -19,6 +19,39 @@
                         <!-- .panel-heading -->
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
+
+                              <!--Sprint 5 -->
+                              <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint5">Sprint - 5</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint5" class="panel-collapse collapse">
+
+                                        <p style="text-align:right"> Temps Sprint : 2 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps passé</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                          <tr>
+                                                                <td>03/01/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Modification site de communication et du tableau Trello</td>
+                                                            </tr>
+
 
                               <!--Sprint 4 -->
                               <div class="panel panel-default">
