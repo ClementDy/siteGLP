cfa87963d4618434244ee0a4feb06ee02d6fe26b
CedricDuhautois <c.duhautois@gmail.com>
2017-01-09 11:57:25 +0100

    Temps passé sprint 5

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                     
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 31 heures</p>
+                    <p style="text-align:right"> Temps Total : 36 heures 30</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,6 +20,51 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 5 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint5">Sprint - 5</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint5" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 5 heures 30</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>04/01/2017</td>
+                                                                <td>3 heures 30</td>
+                                                                <td>Refonte de la couche 'service' de la persistence des données</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>02/01/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Tests sur la couche 'service' de la persistence des données</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <!-- Sprint 4 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
