852ac72a357b44955635743b54bb55957dbbfc2d
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2017-01-17 17:16:24 +0100

    Clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 67 heures</p>
+	                    <p style="text-align:right"> Temps Total : 71 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -22,7 +22,64 @@
 
 
 
-	<!-- Sprint 4 -->
+
+																	<!-- Sprint 6 -->
+																																<div class="panel panel-default">
+																																			<div class="panel-heading">
+																																					<h4 class="panel-title">
+																																							<a data-toggle="collapse" data-parent="#accordion" href="#sprint6">Sprint - 6</a>
+																																					</h4>
+																																			</div>
+																																			<div id="sprint6" class="panel-collapse collapse">
+
+																																					<p style="text-align:right"> Temps Sprint : 4 heures</p>
+																																					<div class="panel panel-default">
+																																							<div class="panel-heading">
+																																									Relev&eacute; d'activit&eacute;s
+																																							</div>
+																																							<!-- /.panel-heading -->
+																																							<div class="panel-body">
+																																									<div class="table-responsive">
+																																											<table class="table table-striped table-bordered table-hover">
+																																													<thead>
+																																															<tr>
+																																																	<th>Date</th>
+																																																	<th>Temps passé</th>
+																																																	<th>T&acirc;che</th>
+																																															</tr>
+																																													</thead>
+																																													<tbody>
+																																														<tr>
+																																															<td>17/01/2017</td>
+																																															<td>1 heure </td>
+																																															<td>1. - Uniformisation du formulaire (design).</td>
+																																														</tr>
+																																														<tr>
+																																															<td>17/01/2017</td>
+																																															<td>1 heure 30</td>
+																																															<td>2.2.1 - Réalisation de la page HTML qui permet au référent de consulter ses offres.</td>
+																																														</tr>
+																																														<tr>
+																																															<td>17/01/2017</td>
+																																															<td>1 heure 30</td>
+																																															<td>2.1 & 2.2 - Création des maquettes pour les CU : "Création d'une offre" & "Consultation des offres"</td>
+																																														</tr>
+
+
+																																													</tbody>
+																																											</table>
+																																									</div>
+																																									<!-- /.table-responsive -->
+																																							</div>
+																																							<!-- /.panel-body -->
+																																					</div>
+																																			</div>
+																																	</div>
+
+
+
+
+	<!-- Sprint 5 -->
 																<div class="panel panel-default">
 																			<div class="panel-heading">
 																					<h4 class="panel-title">
