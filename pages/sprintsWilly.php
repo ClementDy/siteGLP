473a702bf43a61e3808ee0865be259389fd54f04
WillyPezant <pezant.willy@gmail.com>
2017-01-19 16:50:49 +0100

    sprint willy

--- pages/sprintsWilly.php
+++ pages/sprintsWilly.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 36 heures 30</p>
+                    <p style="text-align:right"> Temps Total : 38 heures 30</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Willy
@@ -20,6 +20,52 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
 
+<!-- Sprint 6 -->
+	                              <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        <h4 class="panel-title">
+	                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint6">Sprint - 6</a>
+	                                        </h4>
+	                                    </div>
+	                                    <div id="sprint6" class="panel-collapse collapse">
+
+	                                        <p style="text-align:right"> Temps Sprint : 2 heures</p>
+	                                        <div class="panel panel-default">
+	                                            <div class="panel-heading">
+	                                                Relev&eacute; d'activit&eacute;s
+	                                            </div>
+	                                            <!-- /.panel-heading -->
+	                                            <div class="panel-body">
+	                                                <div class="table-responsive">
+	                                                    <table class="table table-striped table-bordered table-hover">
+	                                                        <thead>
+	                                                            <tr>
+	                                                                <th>Date</th>
+	                                                                <th>Temps passé</th>
+	                                                                <th>T&acirc;che</th>
+	                                                            </tr>
+	                                                        </thead>
+	                                                        <tbody>
+																								 <tr>
+																								  <td>19/01/2016</td>
+																								  <td>1 heure 30</td>
+																								  <td>Modification formulaire candidature - info pour le rendre responsive</td>
+																								</tr>
+																								 <tr>
+																								  <td>19/01/2016</td>
+																								  <td>30 heures</td>
+																								  <td>recherche sur Bootstrap<td>
+																								</tr>
+																								
+	                                                        </tbody>
+	                                                    </table>
+	                                                </div>
+	                                                <!-- /.table-responsive -->
+	                                            </div>
+	                                            <!-- /.panel-body -->
+	                                        </div>
+	                                    </div>
+	                                </div>
 
 							     <!-- Sprint 5 -->
 	                              <div class="panel panel-default">
