bf09f53dadc627ae84aace628c0b4558612348ca
CedricDuhautois <c.duhautois@gmail.com>
2017-01-23 21:22:40 +0100

    Temps passé Sprint 6

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                     
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 40 heures</p>
+                    <p style="text-align:right"> Temps Total : 48 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,6 +20,61 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 6 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint6">Sprint - 6</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint6" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 8 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>20/01/2017</td>
+                                                                <td>2 heures 30</td>
+                                                                <td>Réalisation du formulaire "contrats"</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>20/01/2017</td>
+                                                                <td>1 heure</td>
+                                                                <td>Refactoring global sur le projet</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>19/01/2017</td>
+                                                                <td>2 heures 30</td>
+                                                                <td>Tests sur la gestion des listes d'entités (couche 'présentation')</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>17/01/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Refactoring : Web Services étudiants et formations</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <!-- Sprint 5 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
