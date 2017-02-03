ae703a116be16ed685627f579cfac869a5914999
Lefebvre Alexandre <alexandre3.lefebvre@etudiant.univ-lille1.fr>
2017-02-03 16:27:25 +0100

    heures Alex(2)

--- pages/sprintsAlexandre.php
+++ pages/sprintsAlexandre.php
@@ -11,7 +11,7 @@
 
         <div class="row">
           <div class="col-lg-12">
-            <p style="text-align:right"> Temps Total : 56 heures</p>
+            <p style="text-align:right"> Temps Total : 64 heures</p>
             <div class="panel panel-default">
               <div class="panel-heading">
                 Les sprints d'Alexandre
@@ -28,7 +28,7 @@
                     </div>
                     <div id="sprint7" class="panel-collapse collapse">
 
-                      <p style="text-align:right"> Temps Sprint : 21 heures</p>
+                      <p style="text-align:right"> Temps Sprint : 8 heures</p>
                       <div class="panel panel-default">
                         <div class="panel-heading">
                           Relev&eacute; d'activit&eacute;s
