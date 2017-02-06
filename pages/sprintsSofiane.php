156c27b10dbf459d15b02a0692e8581697518c09
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2017-02-06 11:55:04 +0100

    modif sprint sofiane

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 47 heures</p>
+                    <p style="text-align:right"> Temps Total : 48 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -19,6 +19,48 @@
                         <!-- .panel-heading -->
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
+
+                              <!--Sprint 7 -->
+                              <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint7">Sprint - 7</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint7" class="panel-collapse collapse">
+
+                                        <p style="text-align:right"> Temps Sprint : 1 heure</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps passé</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                          <tr>
+                                                            <td>03/01/2017</td>
+                                                            <td>1 heure </td>
+                                                            <td>Planification des taches pour le nouveau sprint, estimation des taches. Mise à jour des backlogs (entrée et sprint) sur le site de communication</td>
+                                                          </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
 
                               <!--Sprint 6 -->
                               <div class="panel panel-default">
