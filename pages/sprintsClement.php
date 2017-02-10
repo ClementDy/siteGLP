12bb8e633fe833d0fca1046f197bf3887b247966
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2017-02-10 14:51:41 +0100

    Clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 94 heures</p>
+	                    <p style="text-align:right"> Temps Total : 99 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -33,7 +33,7 @@
 																																																			</div>
 																																																			<div id="sprint6" class="panel-collapse collapse">
 
-																																																					<p style="text-align:right"> Temps Sprint : 17 heures</p>
+																																																					<p style="text-align:right"> Temps Sprint : 22 heures</p>
 																																																					<div class="panel panel-default">
 																																																							<div class="panel-heading">
 																																																									Relev&eacute; d'activit&eacute;s
@@ -50,6 +50,16 @@
 																																																															</tr>
 																																																													</thead>
 																																																													<tbody>
+																																																														<tr>
+																																																															<td>10/02/2017</td>
+																																																															<td>4 heures</td>
+																																																															<td>2.2 Modification du controller (JAVA) et de la page WEB (thymeleaf,HTML) pour afficher les offres en attentes de validation d'un référent.</td>
+																																																														</tr>
+																																																														<tr>
+																																																															<td>09/02/2017</td>
+																																																															<td>1 heure</td>
+																																																															<td>2.2 Modification de l'entité referent pour l'accès en base de ses offres.</td>
+																																																														</tr>
 																																																														<tr>
 																																																															<td>08/02/2017</td>
 																																																															<td>5 heures</td>
