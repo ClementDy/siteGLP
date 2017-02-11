2a6923a861b72832c3a304c55a8b0d75d8f9e095
CedricDuhautois <c.duhautois@gmail.com>
2017-02-11 18:28:09 +0100

    Temps passé Sprint 7

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                     
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 53 heures</p>
+                    <p style="text-align:right"> Temps Total : 70 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,6 +20,66 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 7 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint7">Sprint - 7</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint7" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 17 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>10/02/2017</td>
+                                                                <td>3 heures 30</td>
+                                                                <td>Corrections graphiques dans le formulaire "candidature"</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>08/02/2017</td>
+                                                                <td>3 heures</td>
+                                                                <td>Intégration de la validation des types entiers (couche 'présentation')</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>07/02/2017</td>
+                                                                <td>2 heures 30</td>
+                                                                <td>Intégration des composants DatePicker et DateRangePicker</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>05/02/2017</td>
+                                                                <td>3 heures</td>
+                                                                <td>Tests sur la validation des types entiers (couche 'présentation')</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>02/02/2017 - 03/02/2017</td>
+                                                                <td>5 heures</td>
+                                                                <td>Tests sur les composants DatePicker et DateRangePicker</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <!-- Sprint 6 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
