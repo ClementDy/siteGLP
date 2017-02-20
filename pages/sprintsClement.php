81c61689c348c61689d86ed4c43c96714b6a410c
ClementDy <clement.dilly@etudiant.univ-lille1.fr>
2017-02-20 19:42:45 +0100

    Clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 116 heures</p>
+	                    <p style="text-align:right"> Temps Total : 117 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -30,7 +30,7 @@
 																																		</div>
 																																		<div id="sprint8" class="panel-collapse collapse">
 
-																																				<p style="text-align:right"> Temps Sprint : 17 heures</p>
+																																				<p style="text-align:right"> Temps Sprint : 18 heures</p>
 																																				<div class="panel panel-default">
 																																						<div class="panel-heading">
 																																								Relev&eacute; d'activit&eacute;s
@@ -47,6 +47,11 @@
 																																														</tr>
 																																												</thead>
 																																												<tbody>
+																																													<tr>
+																																														<td>20/02/2017</td>
+																																														<td>1 heure</td>
+																																														<td>5.1 - Développement JAVA et WEB pour permettre à un gestionnaire de gérer ses référents (Ajout et affichage des référents)</td>
+																																													</tr>
 																																													<tr>
 																																														<td>16/02/2017</td>
 																																														<td>4 heures</td>
