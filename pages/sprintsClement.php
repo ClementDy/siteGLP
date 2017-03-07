c470c36280bec7ef8b2e6af4b08aea5f553b1206
ClementDy <clement.dilly@etudiant.univ-lille1.fr>
2017-03-07 16:59:37 +0100

    clem

--- pages/sprintsClement.php
+++ pages/sprintsClement.php
@@ -11,7 +11,7 @@
 
 	                    <div class="row">
 	                <div class="col-lg-12">
-	                    <p style="text-align:right"> Temps Total : 131 heures</p>
+	                    <p style="text-align:right"> Temps Total : 133 heures</p>
 	                    <div class="panel panel-default">
 	                        <div class="panel-heading">
 	                            Les sprints de Cl&eacute;ment
@@ -19,6 +19,55 @@
 	                        <!-- .panel-heading -->
 	                        <div class="panel-body">
 	                            <div class="panel-group" id="accordion">
+
+
+
+																																<!-- Sprint 9 -->
+																																															<div class="panel panel-default">
+																																																		<div class="panel-heading">
+																																																				<h4 class="panel-title">
+																																																						<a data-toggle="collapse" data-parent="#accordion" href="#sprint9">Sprint - 9</a>
+																																																				</h4>
+																																																		</div>
+																																																		<div id="sprint9" class="panel-collapse collapse">
+
+																																																				<p style="text-align:right"> Temps Sprint : 2 heures</p>
+																																																				<div class="panel panel-default">
+																																																						<div class="panel-heading">
+																																																								Relev&eacute; d'activit&eacute;s
+																																																						</div>
+																																																						<!-- /.panel-heading -->
+																																																						<div class="panel-body">
+																																																								<div class="table-responsive">
+																																																										<table class="table table-striped table-bordered table-hover">
+																																																												<thead>
+																																																														<tr>
+																																																																<th>Date</th>
+																																																																<th>Temps passé</th>
+																																																																<th>T&acirc;che</th>
+																																																														</tr>
+																																																												</thead>
+																																																												<tbody>
+																																																													<tr>
+																																																														<td>07/03/2017</td>
+																																																														<td>2 heures</td>
+																																																														<td>Gestion des pages d'erreurs (page 404,500).</td>
+																																																													</tr>
+
+
+																																																												</tbody>
+																																																										</table>
+																																																								</div>
+																																																								<!-- /.table-responsive -->
+																																																						</div>
+																																																						<!-- /.panel-body -->
+																																																				</div>
+																																																		</div>
+																																																</div>
+
+
+
+
 
 
 																<!-- Sprint 8 -->
