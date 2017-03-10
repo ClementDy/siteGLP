a0371f66689f6a245db4ae47e6bff5ba12fc5989
WillyPezant <willy.pezant@etudiant.univ-lille1.fr>
2017-03-10 15:43:49 +0100

    will sprint

--- pages/sprintsWilly.php
+++ pages/sprintsWilly.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 91 heures</p>
+                    <p style="text-align:right"> Temps Total : 96 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Willy
@@ -30,7 +30,7 @@
                                                                     </div>
                                                                     <div id="sprint9" class="panel-collapse collapse">
 
-                                                                        <p style="text-align:right"> Temps Sprint : 8 heures</p>
+                                                                        <p style="text-align:right"> Temps Sprint : 13 heures</p>
                                                                         <div class="panel panel-default">
                                                                             <div class="panel-heading">
                                                                                 Relev&eacute; d'activit&eacute;s
@@ -47,6 +47,15 @@
                                                                                             </tr>
                                                                                         </thead>
                                                                                         <tbody>
+											  <tr>
+                                                                                            <td>10/03/2017</td>
+                                                                                            <td>3 heures</td>
+                                                                                            <td>2.3.1 Enregistrement des contrats</td>
+                                                                                          </tr>
+                                                                                            <td>10/03/2017</td>
+                                                                                            <td>2 heures</td>
+                                                                                            <td>2.3.1 Travail page saisie contrat</td>
+                                                                                          </tr>
                                                                                           <tr>
                                                                                             <td>09/03/2017</td>
                                                                                             <td>1 heures</td>
