507ecd5092b61592b617495da292d72050cb3010
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2017-03-16 18:21:53 +0100

    sprint sofiane

--- pages/sprintsSofiane.php
+++ pages/sprintsSofiane.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 77 heures</p>
+                    <p style="text-align:right"> Temps Total : 82 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de Sofiane
@@ -29,7 +29,7 @@
                                     </div>
                                     <div id="sprint9" class="panel-collapse collapse">
 
-                                        <p style="text-align:right"> Temps Sprint : 10 heures</p>
+                                        <p style="text-align:right"> Temps Sprint : 15 heures</p>
                                         <div class="panel panel-default">
                                             <div class="panel-heading">
                                                 Relev&eacute; d'activit&eacute;s
@@ -46,10 +46,17 @@
                                                             </tr>
                                                         </thead>
                                                         <tbody>
+
+                                                          <tr>
+                                                            <td>14/03/2017</td>
+                                                            <td>5 heures</td>
+                                                            <td>Suite à la demande du client, j'ai revu la gestion du CV, possibilité de le visualiser et de le télécharger en tant qu'étudiant mais aussi en tant que reférent dans le profile de l'étudiant</td>
+                                                          </tr>
+
                                                           <tr>
                                                             <td>14/03/2017</td>
                                                             <td>3 heures</td>
-                                                            <td>Probleme résolu avec l'ajout du model attribute dans la methode post du bouton enregistrer mais maintenant il y a le probleme lorsque le CV est vide (affichage dela page d'erreur not found)s</td>
+                                                            <td>Probleme résolu avec l'ajout du model attribute dans la methode post du bouton enregistrer mais maintenant il y a le probleme lorsque le CV est vide (affichage dela page d'erreur not found)</td>
                                                           </tr>
                                                           <tr>
                                                             <td>13/03/2017</td>
