8b526a67435a1673382cec886fe561f9d1176ec5
Lefebvre Alexandre <alexandre3.lefebvre@etudiant.univ-lille1.fr>
2017-03-16 16:59:57 +0100

    heures Alex

--- pages/sprintsAlexandre.php
+++ pages/sprintsAlexandre.php
@@ -11,7 +11,7 @@
 
         <div class="row">
           <div class="col-lg-12">
-            <p style="text-align:right"> Temps Total : 123 heures</p>
+            <p style="text-align:right"> Temps Total : 127 heures</p>
             <div class="panel panel-default">
               <div class="panel-heading">
                 Les sprints d'Alexandre
