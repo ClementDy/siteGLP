44a44c861f6b7bf56c64293969eebdc84a7d1f70
CedricDuhautois <c.duhautois@gmail.com>
2017-03-19 21:08:09 +0100

    Temps passé Sprint 9

--- pages/sprintsCedric.php
+++ pages/sprintsCedric.php
@@ -11,7 +11,7 @@
                     
                     <div class="row">
                 <div class="col-lg-12">
-                    <p style="text-align:right"> Temps Total : 95 heures</p>
+                    <p style="text-align:right"> Temps Total : 115 heures</p>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Les sprints de C&eacute;dric
@@ -20,6 +20,86 @@
                         <div class="panel-body">
                             <div class="panel-group" id="accordion">
                                 
+                                <!-- Sprint 9 -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#sprint9">Sprint - 9</a>
+                                        </h4>
+                                    </div>
+                                    <div id="sprint9" class="panel-collapse collapse">
+                                        <p style="text-align:right"> Temps Sprint : 20 heures</p>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                Relev&eacute; d'activit&eacute;s
+                                            </div>
+                                            <!-- /.panel-heading -->
+                                            <div class="panel-body">
+                                                <div class="table-responsive">
+                                                    <table class="table table-striped table-bordered table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Date</th>
+                                                                <th>Temps pass&eacute;</th>
+                                                                <th>T&acirc;che</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>17/03/2017</td>
+                                                                <td>1 heure</td>
+                                                                <td>Recherche d'offres par type de mission</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>17/03/2017</td>
+                                                                <td>2 heures 30</td>
+                                                                <td>Retour vers le résultat de la recherche depuis la consultation d'une candidature</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>16/03/2017</td>
+                                                                <td>3 heures 30</td>
+                                                                <td>Retour vers le resultat de la recherche depuis la consultation d'une offre</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>14/03/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Corrections pages HTML</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>13/03/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Uniformisation des appels dans les contrôleurs (Repository->Service)</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>10/03/2017</td>
+                                                                <td>1 heure</td>
+                                                                <td>Corrections graphiques (candidature/profil)</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>10/03/2017</td>
+                                                                <td>2 heures</td>
+                                                                <td>Test de validations complémentaires dans le formulaire de candidature</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>10/03/2017</td>
+                                                                <td>3 heures</td>
+                                                                <td>Correction notification d'enregistrement et ajout notification d'erreurs (popup/modal)</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>07/03/2017</td>
+                                                                <td>3 heures</td>
+                                                                <td>Correction de la redirection vers un onglet spécifique du formulaire de candidature</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <!-- /.table-responsive -->
+                                            </div>
+                                            <!-- /.panel-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <!-- Sprint 8 -->
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
